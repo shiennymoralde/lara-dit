ec5d3afacb7c9820eca1f6e3786fe15ab52c7c3c
shiennymoralde <shienny.moralde@holychild.edu.ph>
2022-08-05 11:12:58 +0800

    added

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+@section('content')
+<div class="col-12 mt-5">
+        <h1>Student List</h1>
+        <table class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        </table>
+</div>   
+@endsection
